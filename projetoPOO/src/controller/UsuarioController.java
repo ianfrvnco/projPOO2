cc9a0f8284eb16cbb320d8afeb893ba5e2ef5fde
unknown <Administrador@CSL-N184984.ensino.ulbra.net>
2025-09-01 22:38:40 -0300

    aula 1

---
+++ projetoPOO/src/controller/UsuarioController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author Administrador
+ */
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLDataException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+//import model.Usuario;
+//import utils.Utils;
+
+public class UsuarioController {
+    
+    
+}
---
+++ projetoPOO/src/projetopoo/ProjetoPOO.java
@@ -0,0 +1,17 @@
+
+package projetopoo;
+
+import view.FrLogin;
+
+
+public class ProjetoPOO {
+
+   
+    public static void main(String[] args) {
+       
+        FrLogin telaLogin = new FrLogin();
+        telaLogin.setVisible(true);
+        
+    }
+    
+}
